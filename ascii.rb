6a3e2149c030fddb39b5c46c8c88d0a2de604f13
Hendrik Sollich <Hendrik@hoodie.de>
2013-02-13 17:12:18 +0100

    more comprehensive parsing
    closing of projects possible
    deleting .log and .aux

--- ascii.rb
+++ ascii.rb
@@ -14,10 +14,10 @@ require './lib/options.rb'
 
 ## Checks the existens and creates folder if neccessarry
 def check_projects_folder
-  if File.exists? "#{@options.working_dir}/"
+  if File.exists? "#{@options.working_dir}"
     return true
   else
-    FileUtils.mkdir "#{@options.working_dir}/"
+    FileUtils.mkdir "#{@options.working_dir}"
     puts "Created Projects Directory"
     return false
   end
@@ -31,6 +31,12 @@ def check_project name
     # TODO: create it?
     return false
   end
+end
+
+def close_project name
+  # TODO rename folders
+  FileUtils.mkdir @options.done_dir unless(File.exists? @options.done_dir)
+  FileUtils.mv "#{@options.working_dir}#{name}", @options.done_dir if check_project name
 end
 
 def project_folder name 
@@ -71,27 +77,17 @@ def new_project(name)
 end
 
 ## open project file from name
-def open_project_by_number number 
-  open_project list_projects[number-1]
+def get_project input
+  if (number = input.to_i) != 0
+    @options.projectname = list_projects[number-1]
+  else
+    @options.projectname = input
+  end
 end
 
 ## open project file from name
-def open_project name
+def edit_project name
   edit_file project_file name if check_project name
-end
-
-## list projects
-def list_projects
-  check_projects_folder
-  dir = Dir.entries(@options.working_dir).delete_if { |v| v[0] == '.' }
-  dir
-end
-
-def print_project_list
-    projects = list_projects
-    projects.each_index do |i|
-      puts "#{i+1} #{projects[i]} "
-    end
 end
 
 ## hand path to editor
@@ -101,9 +97,24 @@ def edit_file(path)
   Process.wait pid
 end
 
+## list projects
+def list_projects
+  check_projects_folder
+  dir = Dir.entries(@options.working_dir).delete_if { |v| v[0] == '.' }
+  dir
+end
+
+## pretty version list projects TODO: make prettier
+def print_project_list
+    projects = list_projects
+    projects.each_index do |i|
+      puts "#{i+1} #{projects[i]} "
+    end
+end
 
 ## creates a  latex file from NAME of the desired TYPE
 def write_tex(name, type)
+  return false unless check_project name
   invoicer = Invoicer.new
 
   invoicer.load_templates :invoice => @options.template_invoice , :offer => @options.template_offer
@@ -120,10 +131,12 @@ def write_tex(name, type)
     case type
     when :invoice
       datestr = d['raw_date'].strftime("%Y-%m-%d")
-      file = "#{project_folder name}R#{d['rnumber']} #{name} #{datestr}.tex"
+      filename = "R#{d['rnumber']} #{name} #{datestr}.tex"
+      file = "#{project_folder name}"+filename
     when :offer
       datestr = d['raw_date'].strftime("%y%m%d")
-      file = "#{project_folder name}#{datestr} Angebot #{name}.tex"
+      filename = "#{datestr} Angebot #{name}.tex"
+      file = "#{project_folder name}"+filename
     end
 
     pp file
@@ -139,6 +152,10 @@ def write_tex(name, type)
     puts "Rendering #{file} with #{@options.latex}"
     silencer = @options.verbose ? "" : "> /dev/null" 
     system "#{@options.latex} \"#{file}\" -output-directory . #{silencer}" #TODO output directory is not generic
+    unless @options.keep_log
+      FileUtils.rm filename.gsub('.tex','.log')
+      FileUtils.rm filename.gsub('.tex','.aux')
+    end
   else
     puts "invoice is not valid"
   end
@@ -146,20 +163,26 @@ end
 
 
 ## Use Option parser or leave it if only one argument is given
-if ARGV.size == 1 and ARGV[0][0] != '-'
-  pp ARGV
-  unless ARGV[0].to_i == 0
-    open_project_by_number ARGV[0].to_i
-  else
-    open_project ARGV[0]
+if ARGV.size == 0
+  projects = list_projects
+  projects.each_index do |i|
+    puts "#{i+1} #{projects[i]} "
   end
 else
-  if ARGV.size == 0
-    projects = list_projects
-    projects.each_index do |i|
-      puts "#{i+1} #{projects[i]} "
-    end
-  else
-    @optparse.parse!
-  end
+  @options.projectname = ARGV[0] if ARGV[0][0] != '-'
+  @optparse.parse!
+  @options.projectname = get_project @options.projectname
+  pp @options.operations, @options.projectname, {verbose:@options.verbose} if @options.verbose
+
+  @options.operations = [:edit] if @options.operations.size == 0
+  project = @options.projectname
+  operations = @options.operations
+
+  edit_project project        if operations.include? :edit
+  write_tex project, :invoice if operations.include? :invoice
+  write_tex project, :offer   if operations.include? :offer
+  print_project_list          if operations.include? :list
+  close_project project       if operations.include? :close
+  new_project project         if operations.include? :new
+
 end
--- lib/options.rb
+++ lib/options.rb
@@ -2,11 +2,13 @@
 
 @options                  = OpenStruct.new
 @options.projectname      = nil # leave this way !!
+@options.operations       = [] # :invoice, :offer, :edit, :new
 @options.test             = false
 @options.path             = './'
 @options.editor           = 'vim'
 @options.latex            = 'pdflatex'
 @options.working_dir      = "#{@options.path}projects/"
+@options.done_dir         = "#{@options.path}done/"
 @options.template_dir     = "#{@options.path}templates/"
 @options.template_yml     = "#{@options.template_dir}vorlage.yaml"
 @options.template_invoice = "latex/ascii-rechnung.tex"
@@ -17,47 +19,57 @@
 
 ## OptionsParser
 @optparse = OptionParser.new do|opts|
-
   opts.banner = "Usage: ascii.rb [name|number] or ascii.rb [options]"
+
+  opts.on_tail( '-k', '--keep-log', 'Do not delete latex log and aux files' ) do |name|
+    @options.keep_log = true
+  end
 
   opts.on_tail( '-v', '--verbose', 'Be verbose' ) do |name|
     @options.verbose = true
   end
 
-  opts.on( '-p', '--project NAME', 'Use this project' ) do |name|
+  opts.on( '-p', '--project NAME', 'Use this project (overrides others)' ) do |name|
     @options.projectname = name
   end
 
   opts.on( '-n', '--new NAME', 'Create new Project' ) do |name|
-    new_project @options.projectname = name
+    #new_project @options.projectname = name
+    @options.projectname = @options.projectname.nil? ? name : @options.projectname
+    @options.operations.push :new
   end
 
   opts.on('-e', '--edit [NAME]', "Edit Project") do |name|
-    open_project @options.projectname = name
+    #open_project @options.projectname = name
+    @options.projectname = @options.projectname.nil? ? name : @options.projectname
+    @options.operations.push :edit
   end
 
   opts.on( '-i', '--invoice [NAME]', 'Create invoice from project' ) do |name|
-    project = @options.projectname.nil? ? name : @options.projectname
-    write_tex project, :invoice unless project.nil?
+    @options.projectname = @options.projectname.nil? ? name : @options.projectname
+    @options.operations.push :invoice
+    #write_tex project, :invoice unless project.nil?
   end
 
   opts.on( '-o', '--offer [NAME', 'Create offer from project' ) do |name|
-    project = @options.projectname.nil? ? name : @options.projectname
-    write_tex project, :offer unless project.nil?
+    @options.projectname = @options.projectname.nil? ? name : @options.projectname
+    @options.operations.push :offer
+    #write_tex project, :offer unless project.nil?
   end
 
   opts.on( '-l', '--list', 'List all projects (not implemented)' ) do |name|
-    print_project_list
+    #print_project_list
+    @options.operations.push :list
     exit
   end
 
   opts.on( '--close NAME', 'Close project (no implemented)' ) do |name|
-    puts "--close not yet implemented -- sorry"
-    exit
+    @options.projectname = @options.projectname.nil? ? name : @options.projectname
+    @options.operations.push :close
   end
 
   opts.on( '--clean NAME', 'Removes everything but the project file (not implemented)' ) do |name|
-    puts name
+    puts "--clean is not yet implemented -- sorry"
     exit
   end
 
@@ -68,6 +80,7 @@
 
   opts.on_tail( '-h', '--help', 'Display this screen' ) do
     puts opts
+    @options.operations.push :help
     exit
   end
 end
