a6df58d6bec53f922313862d5214d27aa9b34623
Hendrik Sollich <Hendrik@hoodie.de>
2013-06-24 18:31:42 +0200

    made --dump work again

--- lib/ascii_plumbing.rb
+++ lib/ascii_plumbing.rb
@@ -117,7 +117,7 @@ class ProjectsPlumber
 
 
 
-  def open_project file# or folder ??
+  def open_project file # or folder ??
     case File.ftype file
       when 'file' then
         invoicer = Invoicer.new
