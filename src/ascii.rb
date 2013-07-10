3024475944d65d3f62c2aff9c0f9a82229831a98
Hendrik Sollich <Hendrik@hoodie.de>
2013-07-10 15:57:43 +0200

    started bending my head around thor

--- src/ascii.rb
+++ src/ascii.rb
@@ -6,9 +6,9 @@ require 'optparse'
 require 'ostruct'
 require 'pp'
 require 'paint'
+require 'thor'
 require 'fileutils'
 $SCRIPT_PATH = File.split(File.expand_path(File.readlink(__FILE__)))[0]
-
 require "#{$SCRIPT_PATH}/lib/object.rb"
 require "#{$SCRIPT_PATH}/lib/invoicer.rb"
 require "#{$SCRIPT_PATH}/lib/minizen.rb"
@@ -16,11 +16,56 @@ require "#{$SCRIPT_PATH}/lib/options.rb"
 require "#{$SCRIPT_PATH}/lib/plumber.rb"
 require "#{$SCRIPT_PATH}/lib/ascii_invoicer.rb"
 
+class Commander < Thor
 
-# direkt naming
-@options.projectname = ARGV[0] if ARGV.size > 0 and ARGV[0][0] != '-'
-@optparse.parse!
+  package_name "ascii invoicer"
+  #argument :first, :type => :numeric
+
+  class_option :verbose, :aliases=> "-v", :type => :boolean
+  
+  desc "new FILE", "creating a new project" 
+  def new(file)
+    puts "creating a new project name #{file}"
+  end
+
+  desc "list", "List current Projects"
+  method_option :archives, :type=>:numeric, :aliases => "-a", :lazy_default=> Date.today.year, :required => false, :desc => "List archived Projects"
+  def list
+    unless options[:archives]
+      puts "now listing current projects"
+    else
+      puts "now listing archived projects" , options[:archives]
+    end
+  end
+
+  desc "show NAME", "Show information about the project"
+  def show
+
+  end
+
+  desc "hello NAME", "say hello to NAME"
+  options :from => :required, :yell => :boolean
+  def hello(name)
+    puts "> saying hello" if options[:verbose]
+    output = []
+    output << "from: #{options[:from]}" if options[:from]
+    output << "Hello #{name}"
+    output = output.join("\n")
+    puts options[:yell] ? output.upcase : output
+    puts "> done saying hello" if options[:verbose]
+  end
+
+  desc "default NUMBER","default blub"
+  def default
+    puts options
+  end
+
+end
+
+Commander.start
 
 
-ascii = AsciiInvoicer.new @options
-ascii.execute()
+
+
+#ascii = AsciiInvoicer.new @options
+#ascii.execute()
--- src/lib/options.rb
+++ src/lib/options.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-@version = '1.3.0'
+@version = '1.3.2'
 
 @options                  = OpenStruct.new
 @options.projectname      = nil # leave this way !!
@@ -21,130 +21,129 @@
 @options.keep_log         = false
 @options.verbose          = false
 
-def read_file(datafile)
-  if File.exists?(datafile)
-    file = File.open(datafile)
-    begin
-    data = YAML::load(file)
-    rescue
-      error " error reading #{file}"
-    end
-    return data
-  end
-end
+# def read_file(datafile)
+#   if File.exists?(datafile)
+#     file = File.open(datafile)
+#     begin
+#     data = YAML::load(file)
+#     rescue
+#       error " error reading #{file}"
+#     end
+#     return data
+#   end
+# end
+# 
+# 
+# @settings = read_file "#{$SCRIPT_PATH}/lib/settings.yml"
 
 
-@settings = read_file "#{$SCRIPT_PATH}/lib/settings.yml"
-
-
-## OptionsParser
-@optparse = OptionParser.new do|opts|
-  opts.banner = "Usage: ascii.rb [name|number] or ascii.rb [options]
-    (note that you can always only handle one project)"
-
-  # keep log
-  opts.on_tail( '-k', '--keep-log', 'Do not delete latex log and aux files' ) do |name|
-    @options.keep_log = true
-  end
-
-  # verbose
-  opts.on_tail( '-v', '--verbose', 'Be verbose' ) do |name|
-    @options.verbose = true
-  end
-
-  opts.on_tail( '-V', '--veryverbose', 'Be very verbose' ) do |name|
-    @options.verbose = true
-    @options.veryverbose = true
-  end
-
-  # choose project 
-  opts.on( '-p', '--project NAME', 'Use this project (overrides others)' ) do |name|
-    @options.projectname = name
-  end
-
-  # new project
-  opts.on( '-n', '--new NAME', 'Create new Project' ) do |name|
-    #new_project @options.projectname = name
-    @options.projectname = @options.projectname.nil? ? name : @options.projectname
-    @options.operations.push :new
-  end
-
-  # edit
-  opts.on('-e', '--edit [NAME]', "Edit Project") do |name|
-    #open_project @options.projectname = name
-    @options.projectname = @options.projectname.nil? ? name : @options.projectname
-    @options.operations.push :edit
-  end
-
-  # create invoice
-  opts.on( '-i', '--invoice [NAME]', 'Create invoice from project' ) do |name|
-    @options.projectname = @options.projectname.nil? ? name : @options.projectname
-    @options.operations.push :invoice
-    #write_tex project, :invoice unless project.nil?
-  end
-
-  # create offer
-  opts.on( '-o', '--offer [NAME]', 'Create offer from project' ) do |name|
-    @options.projectname = @options.projectname.nil? ? name : @options.projectname
-    @options.operations.push :offer
-    #write_tex project, :offer unless project.nil?
-  end
-
-  # list projects
-  opts.on( '-l', '--list', 'List all projects' ) do |name|
-    #print_project_list
-    @options.operations.push :list
-  end
-
-  opts.on( '-s', '--sum [NAME]', 'Sum up project sum' ) do |name|
-    @options.projectname = @options.projectname.nil? ? name : @options.projectname
-    @options.operations.push :sum
-    #write_tex project, :offer unless project.nil?
-  end
-
-  opts.on( '-d', '--dump [NAME]', 'Dump raw project data' ) do |name|
-    @options.projectname = @options.projectname.nil? ? name : @options.projectname
-    @options.operations.push :dump
-    #write_tex project, :offer unless project.nil?
-  end
-
-  opts.on( '-f','--file PATH', 'Manualy specify .yml file' ) do |path|
-    @options.project_file = path
-  end
-
-  opts.on_tail( '-h', '--help', 'Display this screen' ) do
-    puts opts
-    @options.operations.push :help
-    exit
-  end
-
-  # close a project
-  opts.on( '--close NAME', 'Close project ' ) do |name|
-    @options.projectname = @options.projectname.nil? ? name : @options.projectname
-    @options.operations.push :archive
-  end
-
-  # close a project
-  opts.on( '--reopen NAME', 'Move closed project back to working dir (Combine with -a)' ) do |name|
-    @options.read_archive = true
-    @options.projectname = @options.projectname.nil? ? name : @options.projectname
-    @options.operations.push :unarchive
-  end
-
-  # list projects
-  opts.on( '-a', '--archive [year]', 'List archived projects (default is current year)' ) do |year|
-    #print_project_list
-    @options.archive_year = year unless year.nil? or year.length == 0
-    @options.read_archive = true
-  end
-
-  ## reopen a project
-  #opts.on( '--reopen NAME', 'Reopen a closed project ' ) do |name|
-  #  exit
-  #  @options.projectname = @options.projectname.nil? ? name : @options.projectname
-  #  @options.operations.push :close
-  #end
-
-end
-
-@optparse.version = @version
+# ## OptionsParser
+# @optparse = OptionParser.new do|opts|
+#   opts.banner = "Usage: ascii.rb [name|number] or ascii.rb [options]
+#     (note that you can always only handle one project)"
+# 
+#   # keep log
+#   opts.on_tail( '-k', '--keep-log', 'Do not delete latex log and aux files' ) do |name|
+#     @options.keep_log = true
+#   end
+# 
+#   # verbose
+#   opts.on_tail( '-v', '--verbose', 'Be verbose' ) do |name|
+#     @options.verbose = true
+#   end
+# 
+#   opts.on_tail( '-V', '--veryverbose', 'Be very verbose' ) do |name|
+#     @options.verbose = true
+#     @options.veryverbose = true
+#   end
+# 
+#   # choose project 
+#   opts.on( '-p', '--project NAME', 'Use this project (overrides others)' ) do |name|
+#     @options.projectname = name
+#   end
+# 
+#   # new project
+#   opts.on( '-n', '--new NAME', 'Create new Project' ) do |name|
+#     #new_project @options.projectname = name
+#     @options.projectname = @options.projectname.nil? ? name : @options.projectname
+#     @options.operations.push :new
+#   end
+# 
+#   # edit
+#   opts.on('-e', '--edit [NAME]', "Edit Project") do |name|
+#     #open_project @options.projectname = name
+#     @options.projectname = @options.projectname.nil? ? name : @options.projectname
+#     @options.operations.push :edit
+#   end
+# 
+#   # create invoice
+#   opts.on( '-i', '--invoice [NAME]', 'Create invoice from project' ) do |name|
+#     @options.projectname = @options.projectname.nil? ? name : @options.projectname
+#     @options.operations.push :invoice
+#     #write_tex project, :invoice unless project.nil?
+#   end
+# 
+#   # create offer
+#   opts.on( '-o', '--offer [NAME]', 'Create offer from project' ) do |name|
+#     @options.projectname = @options.projectname.nil? ? name : @options.projectname
+#     @options.operations.push :offer
+#     #write_tex project, :offer unless project.nil?
+#   end
+# 
+#   # list projects
+#   opts.on( '-l', '--list', 'List all projects' ) do |name|
+#     #print_project_list
+#     @options.operations.push :list
+#   end
+# 
+#   opts.on( '-s', '--sum [NAME]', 'Sum up project sum' ) do |name|
+#     @options.projectname = @options.projectname.nil? ? name : @options.projectname
+#     @options.operations.push :sum
+#     #write_tex project, :offer unless project.nil?
+#   end
+# 
+#   opts.on( '-d', '--dump [NAME]', 'Dump raw project data' ) do |name|
+#     @options.projectname = @options.projectname.nil? ? name : @options.projectname
+#     @options.operations.push :dump
+#     #write_tex project, :offer unless project.nil?
+#   end
+# 
+#   opts.on( '-f','--file PATH', 'Manualy specify .yml file' ) do |path|
+#     @options.project_file = path
+#   end
+# 
+#   opts.on_tail( '-h', '--help', 'Display this screen' ) do
+#     puts opts
+#     @options.operations.push :help
+#     exit
+#   end
+# 
+#   # close a project
+#   opts.on( '--close NAME', 'Close project ' ) do |name|
+#     @options.projectname = @options.projectname.nil? ? name : @options.projectname
+#     @options.operations.push :archive
+#   end
+# 
+#   # close a project
+#   opts.on( '--reopen NAME', 'Move closed project back to working dir (Combine with -a)' ) do |name|
+#     @options.read_archive = true
+#     @options.projectname = @options.projectname.nil? ? name : @options.projectname
+#     @options.operations.push :unarchive
+#   end
+# 
+#   # list projects
+#   opts.on( '-a', '--archive [year]', 'List archived projects (default is current year)' ) do |year|
+#     #print_project_list
+#     @options.archive_year = year unless year.nil? or year.length == 0
+#     @options.read_archive = true
+#   end
+# 
+#   ## reopen a project
+#   #opts.on( '--reopen NAME', 'Reopen a closed project ' ) do |name|
+#   #  exit
+#   #  @options.projectname = @options.projectname.nil? ? name : @options.projectname
+#   #  @options.operations.push :close
+#   #end
+# 
+# end
+# @optparse.version = @version
