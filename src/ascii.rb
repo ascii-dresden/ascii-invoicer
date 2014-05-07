3b8ed8eb635fbaa149aa6707ae2e13d8d0ae49ab
Hendrik Sollich <Hendrik@hoodie.de>
2014-05-07 16:53:30 +0200

    version 2.2.3: fixed bank name in template

--- src/ascii.rb
+++ src/ascii.rb
@@ -418,7 +418,7 @@ class Commander < Thor
     #current = git.log.to_s.lines.to_a.last
     ##puts git.branch unless git.tags.include? current 
     #puts current
-    puts "ascii-invoicer 2.2.1"
+    puts "ascii-invoicer 2.2.3"
   end
 
 
